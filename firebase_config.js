ea320ebc56281cc59ce43184cad743dc80576421
hToko-jp <tokoro.h@geducation.memuro.net>
2026-01-21 14:09:18 +0900

    Add files via upload

---
+++ firebase_config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyB624rigoc0JOVe9e5jac6u8b05wNsUwN0",
+    authDomain: "mine-sansu.firebaseapp.com",
+    databaseURL: "https://mine-sansu-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "mine-sansu",
+    storageBucket: "mine-sansu.firebasestorage.app",
+    messagingSenderId: "1006928664799",
+    appId: "1:1006928664799:web:773849650fe4b89b63716a",
+    measurementId: "G-6KWC16ZWM8"
+};
